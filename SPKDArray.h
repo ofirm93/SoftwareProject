ac383daf88260a7ccb4de3ee186888b218b8b6a6
BarY7 <baryaacovi7@gmail.com>
2017-03-19 17:16:28 +0200

    Update SPKDArray.h
    added size getter and fixed split

--- SPKDArray.h
+++ SPKDArray.h
@@ -11,6 +11,8 @@ typedef struct sp_kd_array_t* SPKDArray;
 
 SPKDArray spInitSPKDArray(SPPoint** pointsArr, int arrSize);
 
-bool spSplitSPKDArray(SPKDArray kdArr, int coor, SPKDArray* kdLeft, SPKDArray* kdRight);
+SPKDArray* spSplitSPKDArray(SPKDArray kdArr, int coor);
+
+int getKDArraySize(SPKDArray array);
 
 #endif //SOFTWAREPROJECT_SPKDARRAY_H
